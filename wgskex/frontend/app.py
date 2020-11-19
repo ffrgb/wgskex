eca5a1d0fdacbacd39a3e38d69f0dea3f5a441bb
Markus Hauschild <markus@moepman.eu>
2020-11-19 17:44:20 +0100

    frontend: pass requests to worker via ZMQ
    Signed-off-by: Markus Hauschild <markus@moepman.eu>

--- wgskex/frontend/app.py
+++ wgskex/frontend/app.py
@@ -1,11 +1,28 @@
+import logging
+
+import zmq
 from fastapi import FastAPI
 
-from wgskex.common import KexInfo
+from wgskex.common import KexInfo, KexResult
 
 app = FastAPI()
 
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+# TODO make the port configurable
+socket.connect("tcp://localhost:5555")
+
+# TODO make loglevel configurable
+logging.basicConfig(format="%(levelname)s: %(message)s", level="DEBUG")
+
 
 @app.post("/api/v1/wg/key/exchange")
-async def wg_key_exchange(kex_info: KexInfo):
-    print(f"Received request for domain {kex_info.domain} with public key: {kex_info.public_key}.")
-    return {"status": "OK"}
+def wg_key_exchange(kex_info: KexInfo):
+    """Submit a public key to be installed into a given mesh domain"""
+    logging.debug(f"Received request for domain {kex_info.domain} with public key: {kex_info.public_key}.")
+    socket.send_pyobj(kex_info)
+    result = socket.recv_pyobj()
+    if isinstance(result, KexResult):
+        return result.dict()
+    else:
+        return {"status": "error", "message": "received invalid response"}
