8c1c1138c05c8731dbe5abd5bf42612b83c12f79
Markus Hauschild <markus@moepman.eu>
2020-12-07 21:22:10 +0100

    frontend: better error handling
    Signed-off-by: Markus Hauschild <markus@moepman.eu>

--- wgskex/frontend/app.py
+++ wgskex/frontend/app.py
@@ -7,11 +7,7 @@ from fastapi import FastAPI
 from wgskex.common import KexInfo, KexResult
 
 app = FastAPI()
-
 context = zmq.Context()
-socket = context.socket(zmq.REQ)
-# TODO make the port configurable
-socket.connect("tcp://localhost:5555")
 
 # TODO make loglevel configurable
 logging.basicConfig(format="%(levelname)s: %(message)s", level="DEBUG")
@@ -21,9 +17,18 @@ logging.basicConfig(format="%(levelname)s: %(message)s", level="DEBUG")
 def wg_key_exchange(kex_info: KexInfo) -> Dict:
     """Submit a public key to be installed into a given mesh domain"""
     logging.debug(f"Received request for domain {kex_info.domain} with public key: {kex_info.public_key}.")
+    socket = context.socket(zmq.REQ)
+    # TODO make the port configurable
+    socket.connect("tcp://localhost:5555")
     socket.send_pyobj(kex_info)
-    result = socket.recv_pyobj()
-    if isinstance(result, KexResult):
-        return result.dict()
+    # TODO make timeout configurable
+    if socket.poll(5000):
+        result = socket.recv_pyobj()
+        if isinstance(result, KexResult):
+            return result.dict()
+        else:
+            logging.warning(f"received invalid response from worker: \"{result!r}\"")
+            return {"status": "error", "message": "received invalid response from worker"}
     else:
-        return {"status": "error", "message": "received invalid response"}
+        logging.warning("timeout waiting for response from worker")
+        return {"status": "error", "message": "timeout waiting for response from worker"}
