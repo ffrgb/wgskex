f1cb7377dec1f4010f4ebb355016078a26c24ffa
Markus Hauschild <markus@moepman.eu>
2021-09-07 21:09:51 +0200

    setup: tag as 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wgskex",
-    version="0.1.0",
+    version="0.2.0",
     author="Markus Hauschild",
     author_email="markus@moepman.eu",
     description="WireGuard Simple Key Exchange",
