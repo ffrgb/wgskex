c4e39202c70784ab584dac2a93cafbd5eb25ab01
Markus Hauschild <markus@moepman.eu>
2021-09-08 21:58:25 +0200

    worker: don't remove never connected peers

--- wgskex/worker/netlink.py
+++ wgskex/worker/netlink.py
@@ -139,7 +139,8 @@ def find_stale_wireguard_clients(wg_interface: str) -> List[str]:
         stale_clients = [
             client.get_attr("WGPEER_A_PUBLIC_KEY").decode("utf-8")
             for client in all_clients
-            if client.get_attr("WGPEER_A_LAST_HANDSHAKE_TIME").get("tv_sec", int()) < three_minutes_ago
+            # TODO add never connected peers to a list and remove them on next call
+            if 0 < client.get_attr("WGPEER_A_LAST_HANDSHAKE_TIME").get("tv_sec", int()) < three_minutes_ago
         ]
 
         return stale_clients
